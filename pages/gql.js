4b443b7e7f8a55572faae2e8b7fa64930c056b94
Abdul Matin Shah <abdulmateenshah@gmail.com>
2021-12-03 17:48:02 +0500

    activated gql

--- pages/gql.js
+++ pages/gql.js
@@ -1,31 +1,31 @@
-// import { gql } from '@apollo/client'
-// import { client } from '../lib/client'
+import { gql } from '@apollo/client'
+import { client } from '../lib/client'
 
-// export default ({ ucs }) => {
-//   return (
-//     <>
-//       <h1>GQLPage</h1>
-//       <pre>{JSON.stringify(ucs, null, 2)}</pre>
-//     </>
-//   )
-// }
+export default ({ ucs }) => {
+  return (
+    <>
+      <h1>GQLPage</h1>
+      <pre>{JSON.stringify(ucs, null, 2)}</pre>
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   const { data } = await client.query({
-//     query: gql`
-//       query MyQuery {
-//         uCs {
-//           edges {
-//             node {
-//               id
-//               title
-//             }
-//           }
-//         }
-//       }
-//     `,
-//   })
-//   return {
-//     props: { ucs: data.uCs.edges },
-//   }
-// }
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query MyQuery {
+        uCs {
+          edges {
+            node {
+              id
+              title
+            }
+          }
+        }
+      }
+    `,
+  })
+  return {
+    props: { ucs: data.uCs.edges },
+  }
+}
